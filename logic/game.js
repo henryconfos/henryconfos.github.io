84641baf87366e54412c03845d47220a096a40a0
Henry Confos <hcon7174@uni.sydney.edu.au>
2022-11-16 21:30:51 +1100

    d

--- logic/game.js
+++ logic/game.js
@@ -22,12 +22,15 @@ var puzzle = {
 
 
 $( ".l-box" ).click(function() {
-    if(this.id == "" || this.id == "incorrect"){
+    if(lastHighlighted == this){
+        this.id = lastHighlighted.id;
+    }
+    if(this.id == "" || this.id == "incorrect" || this.id == "yellow"){
         if(lastHighlighted == undefined){
             this.id = "highlighted";
             lastHighlighted = this;
         }else{
-            if(lastHighlighted.id != "correct" && lastHighlighted.id != "incorrect"){
+            if(lastHighlighted.id != "correct" && lastHighlighted.id != "incorrect" && lastHighlighted.id != "yellow"){
                 lastHighlighted.id = ""
             }
             this.id = "highlighted";
@@ -44,20 +47,47 @@ $( ".letter" ).click(function() {
     }
 })
 
+
+//user can cometimes put two letters in make sure first choice is selected. 
+$(document).keyup(function(e){
+    if(lastHighlighted != undefined){
+        var l = e.key
+        var nL = l.toUpperCase();
+        console.log(nL);
+        guess(nL);
+    }
+})
+
+
+
+
 function guess(letter) {
     $(lastHighlighted).children().text(letter);
 
     var posClass = lastHighlighted.className;
     var pos = posClass.slice(-2);
     var correctLetter = puzzle[pos]
-
+    var pContains = [];
+    for(const l in puzzle){
+        pContains.push(puzzle[l]);
+    }
+    console.log(pContains);
     //Deal with guess
 
     //if correct:
     for(const letters in puzzle){
         if(puzzle[letters] != letter && letter != correctLetter){
-            incorrect();
-            break;
+            //if the letter is in the puzzle but in the wrong spot
+            // letter wrong spot in current word not in puzzle.
+            //TODO: make sure the letter isn't one of the corner ones.
+            if(pContains.indexOf(letter) >= 0){
+                console.log("yellow");
+                yellow();
+                break;
+            }else{
+                incorrect();
+                break;
+            }
         }
         if(puzzle[letters] == letter){
             if(letter == correctLetter){
@@ -73,4 +103,8 @@ function guess(letter) {
 
 function incorrect() {
     lastHighlighted.id = "incorrect";
+}
+
+function yellow() {
+    lastHighlighted.id = "yellow"
 }
